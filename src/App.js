130b7ca8655841022770df9fff87ebeca9dbae0f
Aminul Islam Rocky <mairoky@outlook.com>
2022-11-25 01:20:46 +0600

    404 page added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
+import { Toaster } from 'react-hot-toast';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './routes/Routes';
 import './App.css';
 
 function App() {
   return (
     <div>
-      Hello!
+      <RouterProvider router={router}></RouterProvider>
+      <Toaster></Toaster>
     </div>
   );
 }
--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './NotFound.css';
 
 const NotFound = () => {
     return (
-        <div>
-            Page Not Found
-        </div>
+        <section className='page-note-found'>
+            <div className="container">
+                <div className="row">
+                    <div className="col-10 mx-auto">
+                        <div>
+                            <div className="not-found-bg">
+                                <h1 className="text-center">404</h1>
+                            </div>
+                            <div className="content-box-404 text-center">
+                                <h3>Look like you're lost!</h3>
+                                <p>The page you are looking for not available!</p>
+                                <Link to="/" className="link-404 btn btn-outline-dark">Go to Home</Link>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 };
 
