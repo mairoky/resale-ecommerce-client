8ccd6eedc9911df17b51192337e78942736d84f6
Aminul Islam Rocky <mairoky@outlook.com>
2022-11-26 09:17:05 +0600

    header & footer added

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,87 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaFacebook, FaYoutube, FaTwitter, FaInstagram } from "react-icons/fa";
+import './Footer.css';
 
 const Footer = () => {
     return (
-        <div>
-            Footer
-        </div>
+        <footer>
+            <div className="footer-top py-4">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12">
+                            <div className="about text-center mb-4">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt velit, ratione magni ad quos doloremque accusantium doloribus tempore in sit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt velit, ratione magni ad quos doloremque accusantium doloribus tempore in sit.</p>
+                                <div className="social-media">
+                                    <ul className='list-inline'>
+                                        <li>
+                                            <Link to="/"><FaFacebook /></Link>
+                                        </li>
+                                        <li>
+                                            <Link to="/"><FaInstagram /></Link>
+                                        </li>
+                                        <li>
+                                            <Link to="/"><FaTwitter /></Link>
+                                        </li>
+                                        <li>
+                                            <Link to="/"><FaYoutube /></Link>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-3 col-md-6 col-12">
+                            <div className="footer-info-single">
+                                <h5>Help Center</h5>
+                                <ul className="list-unstyled">
+                                    <li><Link to="/">How to Pay</Link></li>
+                                    <li><Link to="/">FAQ's</Link></li>
+                                    <li><Link to="/">Sitemap</Link></li>
+                                    <li><Link to="/">Delivery Info</Link></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="col-lg-3 col-md-6 col-12">
+                            <div className="footer-info-single">
+                                <h5>Information</h5>
+                                <ul className="list-unstyled">
+                                    <li><Link to="/">About Us</Link></li>
+                                    <li><Link to="/">FAQ's</Link></li>
+                                    <li><Link to="/">Sell Your Items</Link></li>
+                                    <li><Link to="/">Contact Us</Link></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="col-lg-3 col-md-6 col-12">
+                            <div className="footer-info-single">
+                                <h5>Security &amp; privacy</h5>
+                                <ul className="list-unstyled">
+                                    <li><Link to="/">Terms Of Use</Link></li>
+                                    <li><Link to="/">Privacy Policy</Link></li>
+                                    <li><Link to="/">Return / Refund Policy</Link></li>
+                                    <li><Link to="/">Store Locations</Link></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="col-lg-3 col-md-6 col-12">
+                            <div className="footer-info-single">
+                                <h5>Payment</h5>
+                                <p>Sample HTML page with Twitter's Bootstrap. Code example of Easy Sticky Footer using HTML, Javascript, jQuery, and CSS.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="footer-bottom py-3">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12">
+                            <p className="text-center m-0">Copyright © 2023. ReBuy Store.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
     );
 };
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,71 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import { FaSearch } from "react-icons/fa";
+import { Link, NavLink } from 'react-router-dom';
+import './Header.css';
+
 
 const Header = () => {
     return (
-        <div>
-            Header
-        </div>
+        <header className='py-1'>
+            {['lg'].map((expand) => (
+                <Navbar key={expand} expand={expand}>
+                    <Container>
+                        <Navbar.Brand as={Link} to="/">ReBuy Store</Navbar.Brand>
+                        <Navbar.Toggle aria-controls={`rebuy-nav-expand-${expand}`} />
+                        <Navbar.Offcanvas
+                            id={`rebuy-nav-expand-${expand}`}
+                            aria-labelledby={`rebuy-navLabel-expand-${expand}`}
+                            placement="end"
+                        >
+                            <Offcanvas.Header closeButton>
+                                <Offcanvas.Title id={`rebuy-navLabel-expand-${expand}`}>
+                                    ReBuy Store
+                                </Offcanvas.Title>
+                            </Offcanvas.Header>
+                            <Offcanvas.Body>
+                                <Nav className="justify-content-end flex-grow-1">
+                                    <Nav.Link as={Link} to="/">Home</Nav.Link>
+                                    <Nav.Link as={NavLink} to="/">Link</Nav.Link>
+                                </Nav>
+                                <Form className="d-flex">
+                                    <Form.Control
+                                        type="search"
+                                        placeholder="Search"
+                                        className="me-2"
+                                        aria-label="Search"
+                                    />
+                                    <Button variant="outline-dark">
+                                        <FaSearch />
+                                    </Button>
+                                </Form>
+                                <Nav>
+                                    <Nav.Link to="/">Login</Nav.Link>
+                                    <NavDropdown
+                                        title="Profile"
+                                        id={`rebuy-navDropdown-expand-${expand}`}
+                                    >
+                                        <NavDropdown.Item as={Link} to="/">
+                                            Dashboard
+                                        </NavDropdown.Item>
+                                        <NavDropdown.Divider />
+                                        <NavDropdown.Item as={Link} to="/">
+                                            Logout
+                                        </NavDropdown.Item>
+                                    </NavDropdown>
+                                </Nav>
+                            </Offcanvas.Body>
+                        </Navbar.Offcanvas>
+                    </Container>
+                </Navbar>
+            ))}
+        </header>
     );
 };
 
